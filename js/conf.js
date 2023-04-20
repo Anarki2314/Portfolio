fd0e3aa9411b8cf22b11189a650fe18fdd2931a3
Anarki <65044870+Anarki2314@users.noreply.github.com>
2023-04-20 21:04:51 +0300

    Add files via upload

---
+++ js/conf.js
@@ -0,0 +1,137 @@
+const menu_list=
+{
+    "main": "lorem",
+    "about": "ipsum",
+    "contacts": "322",
+    "profile": "228",
+}
+
+const type={
+    "type":{
+        "WEB":['sites', 'templates'],
+
+        "arts":['rastre', 'vector'],
+    },
+    "subtype":{
+        "sites_WEB":['2023','2022'],
+        "templates_WEB":['2023'],
+
+        "rastre_arts":['2023'],
+        "vector_arts":['2023'],
+    },
+
+
+    "year":{
+
+        "2023_sites":['FastPC'],
+        "2023_templates":[
+            'Popart',
+            'Avangard',
+            'Brutalism',
+            'Diary main',
+            'Diary tasks',
+            'Diary calendar',
+            'Diary contacts',
+        ],
+
+
+        "2022_sites":['Beautify','VEDExpress','My-Giro Dark', 'My-Giro Light'],
+
+
+        "2023_rastre":
+        [
+            'Сolor palette',
+            'Collage 1',
+            'Collage 2',
+            'Collage 3',
+            'Calendar pocket',
+            'Calendar desk',
+            'Calendar wall 1',
+            'Calendar wall 2',
+            'Calendar wall 3',
+            'Calendar wall 4',
+            'Сomposition',
+            'Eastern wisdom 1',
+            'Eastern wisdom 2',
+            'Time',
+            'Business card',
+            'Business card rev',
+            'Illusion',
+            'Mark 1',
+            'Mark 2',
+            'Mark 3',
+            '3d book 1',
+            '3d book 2',
+            '3d book 3',
+
+
+        ],
+        "2023_vector":[
+            'Rebus',
+            'Labyrinth',
+            'Postcard',
+            'Postcard rev',
+            'Logo',
+
+        ],
+
+
+    },
+    "work":{
+
+
+        // WEB
+        "FastPC_2023": ['Lorem Ipsum'],
+        "Popart_2023": ['Lorem Ipsum'],
+        "Avangard_2023": ['Lorem Ipsum'],
+        "Brutalism_2023": ['Lorem Ipsum'],
+        "Diary main_2023": ['Lorem Ipsum'],
+        "Diary tasks_2023": ['Lorem Ipsum'],
+        "Diary calendar_2023": ['Lorem Ipsum'],
+        "Diary contacts_2023": ['Lorem Ipsum'],
+
+
+
+        "Beautify_2022": ['Lorem Ipsum'],
+        "VEDExpress_2022": ['Lorem Ipsum'],
+        "My-Giro Dark_2022": ['Lorem Ipsum'],
+        "My-Giro Light_2022": ['Lorem Ipsum'],
+        "My-Giro Light_2022": ['Lorem Ipsum'],
+
+
+        // ARTS
+
+        "Сolor palette_2023": ['Lorem Ipsum'],
+        "Collage 1_2023": ['Lorem Ipsum'],
+        "Collage 2_2023": ['Lorem Ipsum'],
+	"Collage 3_2023": ['Lorem Ipsum'],
+        "Calendar pocket_2023": ['Lorem Ipsum'],
+        "Calendar desk_2023": ['Lorem Ipsum'],
+        "Calendar wall 1_2023": ['Lorem Ipsum'],
+        "Calendar wall 2_2023": ['Lorem Ipsum'],
+        "Calendar wall 3_2023": ['Lorem Ipsum'],
+        "Calendar wall 4_2023": ['Lorem Ipsum'],
+        "Сomposition_2023": ['Lorem Ipsum'],
+        "Eastern wisdom 1_2023": ['Lorem Ipsum'],
+        "Eastern wisdom 2_2023": ['Lorem Ipsum'],
+        "Time_2023": ['Lorem Ipsum'],
+        "Business card_2023": ['Lorem Ipsum'],
+        "Business card rev_2023": ['Lorem Ipsum'],
+        "Illusion_2023": ['Lorem Ipsum'],
+        "Rebus_2023": ['Lorem Ipsum'],
+        "Labyrinth_2023": ['Lorem Ipsum'],
+        "Postcard_2023": ['Lorem Ipsum'],
+        "Postcard rev_2023": ['Lorem Ipsum'],
+        "Logo_2023": ['Lorem Ipsum'],
+        "Mark 1_2023": ['Lorem Ipsum'],
+        "Mark 2_2023": ['Lorem Ipsum'],
+        "Mark 3_2023": ['Lorem Ipsum'],
+        "3d book 1_2023": ['Lorem Ipsum'],
+        "3d book 2_2023": ['Lorem Ipsum'],
+        "3d book 3_2023": ['Lorem Ipsum'],
+
+        // "FastPC_2023": ['Lorem Ipsum'],
+        // "FastPC_2023": ['Lorem Ipsum'],
+        // "FastPC_2023": ['Lorem Ipsum'],
+    }
+}
